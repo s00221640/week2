44b15bfead0b246e57dd0ac396fc647a5c493905
s00221640 <s00221640@atu.ie>
2024-01-30 16:57:55 +0000

    slide 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'week2';
+  thingsToDo = [
+    'Learn javascript',
+    'Learn Angular',
+    'Learn Redux'
+  ];
 }
